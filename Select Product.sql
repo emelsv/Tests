d245019aa039109e3381592a9f0e88726b836dc3
Sergey <emel_sv@mail.ru>
2016-08-17 21:33:40 +0300

    Selected Product 2

--- Select Product.sql
+++ Select Product.sql
@@ -1,7 +1,23 @@
-select p.ProductId
-      ,count(p.RowNumber) as NumberFirstSales
-from (select ProductId
-            ,row_number() over (partition by CustomerId order by DateCreated asc) as RowNumber
-      from Sales) p
-where p.RowNumber = 1
-group by p.ProductId
+select p.ProductId 
+      ,count(p.RowNumber) as NumberFirstSales 
+from (select ProductId 
+            ,row_number() over (partition by CustomerId order by DateCreated asc) as RowNumber 
+      from Sales) p 
+where p.RowNumber = 1 
+group by p.ProductId 
+
+select s.ProductId, count(s.ProductId) as NumberFirstSales 
+from Sales s
+inner join (select CustomerId
+                  ,min(DateCreated) as MinDateCreated
+            from Sales
+            group by CustomerId) c on s.CustomerId = c.CustomerId and c.MinDateCreated = s.DateCreated
+group by s.ProductId
+
+select s.ProductId, count(s.ProductId) as NumberFirstSales 
+from Sales s
+inner join (select CustomerId
+                  ,min(Id) as MinId
+            from Sales
+            group by CustomerId) c on s.CustomerId = c.CustomerId and c.MinId = s.Id
+group by s.ProductId
