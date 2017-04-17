0abae40b08b2f5d9c381e134a6d0b0bfa3d181e1
emelsv <emel_sv@mail.ru>
2017-04-17 12:59:40 +0400

    Add files via upload

--- ProgramEventExample.cs
+++ ProgramEventExample.cs
@@ -4,51 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication3
+namespace ConsoleApplication7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CTest ct = new CTest();
-            ct.ValueChangeHandler += Program_ValueChanged;
+            TestEventPublisher tep = new TestEventPublisher();
+            tep.ValueChanged += Program_OnValueChanged;
 
-            ct.Value = 100;
-            ct.Value = 200;
-            ct.Value = 300;
+            tep.Value = 0;
+            tep.Value = 1;
+            tep.Value = 2;
 
-            Console.ReadLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
 
-        public static void Program_ValueChanged(object sender, ValueChangedEventArgs e)
+        static void Program_OnValueChanged(object sender, TestEventsArgs e)
         {
-            Console.WriteLine("Event  handled.");
+            Console.WriteLine("Old value = {0}, new value = {1}", e.OldValue, e.NewValue);
         }
     }
 
-    public class CTest
+
+    public class TestEventsArgs : EventArgs
+    {
+        public int OldValue { get; set; }
+        public int NewValue { get; set; }
+    }
+
+    public class TestEventPublisher
     {
         private int _value;
 
-        public event EventHandler<ValueChangedEventArgs> ValueChangeHandler;
-        public int Value 
-        { 
-            get { return _value; } 
+        public event EventHandler<TestEventsArgs> ValueChanged;
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
             set
             {
-                ValueChangedEventArgs ea = new ValueChangedEventArgs();
-                _value = value;
-                OnValueChanged(ea);
-            } 
+                if (_value != value)
+                {
+                    TestEventsArgs ea = new TestEventsArgs();
+                    ea.OldValue = _value;
+                    _value = value;
+                    ea.NewValue = _value;
+                    OnValueChanged(ea);
+                }
+            }
         }
 
-        protected virtual void OnValueChanged(ValueChangedEventArgs e)
+        protected virtual void OnValueChanged(TestEventsArgs e)
         {
-            ValueChangeHandler?.Invoke(this, e);
+            ValueChanged?.Invoke(this, e);
         }
-    }
-
-    public class ValueChangedEventArgs : EventArgs
-    {
     }
 }
