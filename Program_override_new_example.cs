6cf8fff6745ada8b3d2e883c0eb121786d832692
emelsv <emel_sv@mail.ru>
2017-05-12 11:54:11 +0400

    Add files via upload

---
+++ Program_override_new_example.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Test();
+
+            A b = new B();
+            b.Test();
+
+            A c = new C();
+            c.Test();
+
+            C c1 = new C();
+            c1.Test();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+
+    public class A
+    {
+        public virtual void Test()
+        {
+            Console.WriteLine("Class A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Test()
+        {
+            Console.WriteLine("Class B");
+        }
+    }
+
+    public class C : A
+    {
+        public new void Test()
+        {
+            Console.WriteLine("Class C");
+        }
+    }
+}
