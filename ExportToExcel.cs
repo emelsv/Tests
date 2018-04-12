7cb18d761acad8754a663ddbbda7c488f965e6f8
emelsv <emel_sv@mail.ru>
2018-04-12 18:08:25 +0300

    Add files via upload

---
+++ ExportToExcel.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Data;
+using System.Reflection;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using System.Threading.Tasks;
+
+namespace QuantumsSupplies.Excel
+{
+    public class ExportToExcelFile
+    {
+
+#region MY_REGION
+
+        public static Task<bool> Execute<T>(List<T> list, string listName, string[] columnsNames, string xlsxFilePath)
+        {
+            return Task.Factory.StartNew<bool>(() =>
+                {
+                    try
+                    {
+                        using (SpreadsheetDocument document = SpreadsheetDocument.Create(xlsxFilePath, SpreadsheetDocumentType.Workbook))
+                        {
+                            WriteExcelFile<T>(list, listName, columnsNames, document);
+                        }
+                        return true;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                });
+        }
+
+        private static void WriteExcelFile<T>(List<T> list, string listName, string[] columnsNames, SpreadsheetDocument spreadsheet)
+        {
+            spreadsheet.AddWorkbookPart();
+            spreadsheet.WorkbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+
+            spreadsheet.WorkbookPart.Workbook.Append(new BookViews(new WorkbookView()));
+
+            WorkbookStylesPart workbookStylesPart = spreadsheet.WorkbookPart.AddNewPart<WorkbookStylesPart>("rIdStyles");
+            Stylesheet stylesheet = new Stylesheet();
+            workbookStylesPart.Stylesheet = stylesheet;
+
+            uint worksheetNumber = 1;
+            string workSheetID = "rId" + worksheetNumber.ToString();
+            string worksheetName = listName;
+
+            WorksheetPart newWorksheetPart = spreadsheet.WorkbookPart.AddNewPart<WorksheetPart>();
+            newWorksheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet();
+
+            newWorksheetPart.Worksheet.AppendChild(new DocumentFormat.OpenXml.Spreadsheet.SheetData());
+
+            WriteListToExcelWorksheet<T>(list, columnsNames, newWorksheetPart);
+            newWorksheetPart.Worksheet.Save();
+
+            spreadsheet.WorkbookPart.Workbook.AppendChild(new DocumentFormat.OpenXml.Spreadsheet.Sheets());
+
+            spreadsheet.WorkbookPart.Workbook.GetFirstChild<DocumentFormat.OpenXml.Spreadsheet.Sheets>().AppendChild(new DocumentFormat.OpenXml.Spreadsheet.Sheet()
+            {
+                Id = spreadsheet.WorkbookPart.GetIdOfPart(newWorksheetPart),
+                SheetId = (uint)worksheetNumber,
+                Name = listName
+            });
+
+            worksheetNumber++;
+
+            spreadsheet.WorkbookPart.Workbook.Save();
+        }
+
+        private static void WriteListToExcelWorksheet<T>(List<T> list, string[] columnsNames, WorksheetPart worksheetPart)
+        {
+            var worksheet = worksheetPart.Worksheet;
+            var sheetData = worksheet.GetFirstChild<SheetData>();
+
+            string cellValue = "";
+
+            DataTable dt = ListToDataTable<T>(list);
+
+            int numberOfColumns = columnsNames.Count(); //result.Columns.Count;
+            bool[] IsNumericColumn = new bool[numberOfColumns];
+
+            string[] excelColumnNames = new string[numberOfColumns];
+            for (int n = 0; n < numberOfColumns; n++)
+                excelColumnNames[n] = GetExcelColumnName(n);
+
+            uint rowIndex = 1;
+
+            var headerRow = new Row { RowIndex = rowIndex };
+            sheetData.Append(headerRow);
+
+            for (int colInx = 0; colInx < numberOfColumns; colInx++)
+            {
+                DataColumn col = dt.Columns[colInx];
+                AppendTextCell(excelColumnNames[colInx] + "1", columnsNames[colInx], headerRow);
+                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
+            }
+
+            double cellNumericValue = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                ++rowIndex;
+                var newExcelRow = new Row { RowIndex = rowIndex };
+                sheetData.Append(newExcelRow);
+
+                for (int colInx = 0; colInx < numberOfColumns; colInx++)
+                {
+                    cellValue = dr.ItemArray[colInx].ToString();
+
+                    if (IsNumericColumn[colInx])
+                    {
+                        cellNumericValue = 0;
+                        if (double.TryParse(cellValue, out cellNumericValue))
+                        {
+                            cellValue = cellNumericValue.ToString();
+                            AppendNumericCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, newExcelRow);
+                        }
+                    }
+                    else
+                    {
+                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, newExcelRow);
+                    }
+                }
+            }
+        }
+#endregion
+
+        #region HELPER_FUNCTIONS
+        public static DataTable ListToDataTable<T>(List<T> list)
+        {
+            DataTable dt = new DataTable();
+
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                dt.Columns.Add(new DataColumn(info.Name, GetNullableType(info.PropertyType)));
+            }
+            foreach (T t in list)
+            {
+                DataRow row = dt.NewRow();
+                foreach (PropertyInfo info in typeof(T).GetProperties())
+                {
+                    if (!IsNullableType(info.PropertyType))
+                        row[info.Name] = info.GetValue(t, null);
+                    else
+                        row[info.Name] = (info.GetValue(t, null) ?? DBNull.Value);
+                }
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+        private static Type GetNullableType(Type t)
+        {
+            Type returnType = t;
+            if (t.IsGenericType && t.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                returnType = Nullable.GetUnderlyingType(t);
+            }
+            return returnType;
+        }
+        private static bool IsNullableType(Type type)
+        {
+            return (type == typeof(string) ||
+                    type.IsArray ||
+                    (type.IsGenericType &&
+                     type.GetGenericTypeDefinition().Equals(typeof(Nullable<>))));
+        }
+        #endregion
+
+
+        private static void AppendTextCell(string cellReference, string cellStringValue, Row excelRow)
+        {
+            Cell cell = new Cell() { CellReference = cellReference, DataType = CellValues.String };
+            CellValue cellValue = new CellValue();
+            cellValue.Text = cellStringValue;
+            cell.Append(cellValue);
+            excelRow.Append(cell);
+        }
+
+        private static void AppendNumericCell(string cellReference, string cellStringValue, Row excelRow)
+        {
+            Cell cell = new Cell() { CellReference = cellReference };
+            CellValue cellValue = new CellValue();
+            cellValue.Text = cellStringValue;
+            cell.Append(cellValue);
+            excelRow.Append(cell);
+        }
+
+        private static string GetExcelColumnName(int columnIndex)
+        {
+            if (columnIndex < 26)
+                return ((char)('A' + columnIndex)).ToString();
+
+            char firstChar = (char)('A' + (columnIndex / 26) - 1);
+            char secondChar = (char)('A' + (columnIndex % 26));
+
+            return string.Format("{0}{1}", firstChar, secondChar);
+        }
+    }
+}
